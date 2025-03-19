576060e650a8af31a42168b68fc307534b1f216f
SerenityFlaim <phil_th@mail.ru>
2025-03-19 10:26:54 +0300

    f#_intro|digit sum via tail recursion

--- Program.fs
+++ Program.fs
@@ -15,8 +15,8 @@ let solve a b c =
         else Quadratic(((-b+sqrt(D))/(2.0*a), (-b-sqrt(D))/(2.0*a)))
 
 let circleSurface r = Math.PI * r * r
-
 let multiplySurfaceH s h = s * h
+
 let cylinderVolumeSuperPosition = circleSurface >> multiplySurfaceH
 let cylinderVolumeCurry r h = (circleSurface r) * h
 
@@ -25,6 +25,14 @@ let rec digitSumUp num =
     if num = 0 then 0
     else (num % 10) + (digitSumUp(num / 10))
 
+let digitSumDown num = 
+    let rec digitSumTail num acc = 
+        if num = 0 then acc
+        else
+            let digit = num % 10
+            let cur_num = num / 10
+            digitSumTail cur_num (acc + digit)
+    digitSumTail num 0
 
 [<EntryPoint>]
 let main (argv :string[]) = 
@@ -69,4 +77,13 @@ let main (argv :string[]) =
 
     let sum = digitSumUp num
     printfn "Digit sum of %d is: %d" num sum
+
+    Console.WriteLine("<<----------------->>")
+    Console.WriteLine("Digit Sum (Recursion DOWN)")
+
+    Console.WriteLine("Enter number:")
+    let num2 = Console.ReadLine() |> int
+
+    let sum2 = digitSumDown num2
+    printfn "Digit sum of %d is: %d" num2 sum2
     0
