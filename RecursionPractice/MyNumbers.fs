dc9c8b4d74412293835e697728787e064c249c10
SerenityFlaim <phil_th@mail.ru>
2025-03-26 14:49:20 +0300

    f#_rec|task 19 - MyNumbers module functions optimized

--- RecursionPractice/MyNumbers.fs
+++ RecursionPractice/MyNumbers.fs
@@ -1,41 +1,52 @@
 module MyNumbers
+open Numbers
+open Euler
 open System
 
-let rec gcd2 x y = 
+let rec gcd x y = 
     match y with
     | 0 -> x
-    | _ -> gcd2 y (x % y)
+    | _ -> gcd y (x % y)
+
+// let countPrimeDivisors number = 
+//     let rec countTail number acc temp = 
+//         match temp with
+//         | 0 -> acc
+//         | _ -> 
+//             let newAcc = if gcd number temp = 1 then acc+1 else acc
+//             countTail number newAcc (temp-1)
+//     countTail number 0 number
 
 let countPrimeDivisors number = 
-    let rec countTail number acc temp = 
-        match temp with
-        | 0 -> acc
-        | _ -> 
-            let newAcc = if gcd2 number temp = 1 then acc+1 else acc
-            countTail number newAcc (temp-1)
-    countTail number 0 number
+    EulerFunction number
+
+// let countDigitDivisors3 number = 
+//     let rec countTail number acc = 
+//         match number with
+//         | 0 -> acc
+//         | n ->
+//             let digit = number % 10
+//             let next = number / 10
+//             let newAcc = if digit % 3 = 0 then acc+digit else acc
+//             countTail next newAcc
+//     countTail number 0
 
 let countDigitDivisors3 number = 
-    let rec countTail number acc = 
-        match number with
-        | 0 -> acc
-        | n ->
-            let digit = number % 10
-            let next = number / 10
-            let newAcc = if digit % 3 = 0 then acc+digit else acc
-            countTail next newAcc
-    countTail number 0
+    digitTraverseCondition number (+) 0 (fun x -> x % 3 = 0)
+
+// let primeDigitTraverse number div = 
+//     let rec primeTail number div acc = 
+//         match number with
+//         | 0 -> acc
+//         | _ -> 
+//             let digit = number % 10
+//             let next = number / 10
+//             let newAcc = if gcd digit div = 1 then acc + 1 else acc
+//             primeTail next div newAcc
+//     primeTail number div 0
 
 let primeDigitTraverse number div = 
-    let rec primeTail number div acc = 
-        match number with
-        | 0 -> acc
-        | _ -> 
-            let digit = number % 10
-            let next = number / 10
-            let newAcc = if gcd2 digit div = 1 then acc + 1 else acc
-            primeTail next div newAcc
-    primeTail number div 0
+    digitTraverseCondition number (fun x y -> x+1) 0 (fun x -> gcd x div = 1)
 
 let findBestDivisor number = 
     let rec findTail div bestDiv bestCount = 
