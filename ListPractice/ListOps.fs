cc918715e99f1175b15300bb230af8d392b2ba9f
SerenityFlaim <phil_th@mail.ru>
2025-04-01 19:41:02 +0300

    f#_lists|tasks 1-7

---
+++ ListPractice/ListOps.fs
@@ -0,0 +1,86 @@
+module ListOps
+open System
+
+let readList n  =  
+    let rec readListTail n acc = 
+        match n with
+        | 0 -> acc
+        | k -> 
+            let el = Console.ReadLine() |> Int32.Parse
+            let newAcc = acc@[el]
+            readListTail (k-1) newAcc
+    readListTail n []
+
+let rec printList l = 
+    match l with
+    | [] -> Console.ReadKey()
+    | head::tail ->
+        Console.WriteLine(Int32.Parse(head))
+        printList tail
+
+let rec listTraverseCondition list (func :int->int->int) (cond :int->bool) acc  = 
+    match list with
+    | [] -> acc
+    | head::tail -> 
+        let flag = cond head
+        match flag with
+        | true -> listTraverseCondition tail func cond (func acc head)
+        | false -> listTraverseCondition tail func cond acc
+
+
+let findMinElement list = 
+    match list with
+    | [] -> 0
+    | head::tail -> listTraverseCondition tail (fun x y -> if x < y then x else y) (fun x -> true) head
+
+let findEvenSum list = 
+    listTraverseCondition list (+) (fun x -> x%2=0) 0
+
+let countUneven list = 
+    listTraverseCondition list (fun x y -> x+1) (fun x -> x%2=1) 0
+
+let maxList list =
+    listTraverseCondition list (fun x y -> if x > y then x else y) (fun x -> true) System.Int32.MinValue
+
+let rec freqElement list element count =
+    match list with
+    | [] -> count
+    | head :: tail ->
+        match head=element with
+        | false -> freqElement tail element count
+        | true -> freqElement tail element (count+1)
+
+let rec freqList list mainList currentList =
+    match list with
+    | [] -> currentList
+    | head :: tail -> 
+        let freqEl = freqElement mainList head 0
+        freqList tail mainList (currentList @ [freqEl])
+
+let pos list el = 
+    let rec pos1 list el num = 
+        match list with
+            |[] -> 0
+            |h::t ->    
+                if (h = el) then num
+                else 
+                    let num1 = num + 1
+                    pos1 t el num1
+    pos1 list el 1
+
+let getIn list pos = 
+    let rec getIn1 list num curNum = 
+        match list with 
+            |[] -> 0
+            |h::t -> 
+                if num = curNum then h
+                else 
+                    let newNum = curNum + 1
+                    getIn1 t num newNum
+    getIn1 list pos 1
+
+let frequencyElement list = 
+    let fL = freqList list list []
+    let maxFreq = maxList fL
+    let index = pos fL maxFreq
+    getIn list index
---
+++ ListPractice/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open ListOps
+
+[<EntryPoint>]
+let main argv =
+    let arr = [1; 2; 7; 7; 7; 2; 5; 3; 4]
+
+    Console.WriteLine("<--->")
+    let res = listTraverseCondition arr (+) (fun x -> x%2=0) 0
+    Console.WriteLine(res)
+
+    let min = findMinElement arr
+    Console.WriteLine(min)
+
+    let un_res = countUneven arr
+    Console.WriteLine(un_res)
+
+    Console.WriteLine("<------------->")
+    let most_freq = frequencyElement arr
+    Console.WriteLine("Most freak element:")
+    Console.WriteLine(most_freq)
+    0
