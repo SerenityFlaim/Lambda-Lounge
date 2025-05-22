f6dc8b10583886ecade8ce78cad5b087794aa909
SerenityFlaim <phil_th@mail.ru>
2025-05-22 15:01:28 +0300

    prolog_52| solve method

--- prolog/52/task52.pl
+++ prolog/52/task52.pl
@@ -13,3 +13,38 @@ solution([X1, X2, X3, X4, X5]) :-
     my_between(1, 4, X4),        
     X5 is RemainingSum2 - X4, 
     my_between(1, 3, X5).         
+
+my_between(Low, High, Value) :-
+    integer(Low),
+    integer(High),
+    Low =< High,               
+    (Value = Low ; NewLow is Low + 1,     
+     my_between(NewLow, High, Value)
+    ).
+
+my_findall(Template, Goal, Result) :-
+    call(Goal), 
+    assertz(temp_result(Template)), 
+    fail.
+
+my_findall(_, _, Result) :-
+    collect_results([], Result).
+
+collect_results(Acc, Result) :-
+    retract(temp_result(X)),
+    !,
+    collect_results([X | Acc], Result).
+
+collect_results(Result, Result). % Когда результаты закончились, возвращаем аккумулятор
+
+write_solutions([], _).
+write_solutions([Solution | Rest], Stream) :-
+    write(Stream, Solution),
+    nl(Stream),
+    write_solutions(Rest, Stream).
+
+solve :-
+    my_findall([X1, X2, X3, X4, X5], solution([X1, X2, X3, X4, X5]), Solutions),
+    open('/home/serenity-flaim/Desktop/ФКТиПМ/Summer_semester/ФиЛП/LambdaLounge/FSharp/prolog/52/solutions.txt', write, Stream),
+    write_solutions(Solutions, Stream),
+    close(Stream).
